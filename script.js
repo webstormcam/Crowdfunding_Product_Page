87dcc8f8842ae1b5c2a5ee62a55b48ac0e05d506
Cameron Chandler <webstormcam@gmail.com>
2022-09-07 20:50:24 -0400

    Building out the first part of the main-section

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const ham = document.getElementById('ham')
 const miniMenu = document.getElementById('mini-option-menu')
+const header = document.querySelector('header')
 
 ham.addEventListener('click',function(){
 if(ham.getAttribute('src')==='imgs/icon-hamburger.svg'){
     ham.src='imgs/icon-close-menu.svg'
     miniMenu.style.display='flex'
+    header.classList.add('shade')
 } else{
     ham.src='imgs/icon-hamburger.svg'
     miniMenu.style.display='none'
+    header.classList.remove('shade')
 }
 });
